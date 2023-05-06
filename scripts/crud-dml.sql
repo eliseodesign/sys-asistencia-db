58680334393180d4207f71139317433bcc5e4776
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-05-06 12:15:59 -0600

    [:recycle: refactor] restructure scripts

---
+++ scripts/crud-dml.sql
@@ -0,0 +1,54 @@
+-- Grupo
+INSERT INTO Grupo (Nombre) VALUES ('Grupo1');
+UPDATE Grupo SET Nombre = 'Grupo2' WHERE Id = 1;
+DELETE FROM Grupo WHERE Id = 1;
+SELECT * FROM Grupo;
+
+-- Anio
+INSERT INTO Anio (Nombre) VALUES ('Anio1');
+UPDATE Anio SET Nombre = 'Anio2' WHERE Id = 1;
+DELETE FROM Anio WHERE Id = 1;
+SELECT * FROM Anio;
+
+-- GrupoAnio
+INSERT INTO GrupoAnio (IdGrupo, IdAnio) VALUES (1, 1);
+UPDATE GrupoAnio SET IdGrupo = 2 WHERE Id = 1;
+DELETE FROM GrupoAnio WHERE Id = 1;
+SELECT * FROM GrupoAnio;
+
+-- Turno
+INSERT INTO Turno (Nombre) VALUES ('Turno1');
+UPDATE Turno SET Nombre = 'Turno2' WHERE Id = 1;
+DELETE FROM Turno WHERE Id = 1;
+SELECT * FROM Turno;
+
+-- Carrera
+INSERT INTO Carrera (Nombre, Siglas) VALUES ('Carrera1', 'C1');
+UPDATE Carrera SET Nombre = 'Carrera2' WHERE Id = 1;
+DELETE FROM Carrera WHERE Id = 1;
+SELECT * FROM Carrera;
+
+-- GrupoAnioCarrera
+INSERT INTO GrupoAnioCarrera (Codigo, EstudiantesMax, DiasPresencial, IdCarrera, IdGrupoAnio) VALUES ('GAC1', 30, 1, 1, 1);
+UPDATE GrupoAnioCarrera SET Codigo = 'GAC2' WHERE Id = 1;
+DELETE FROM GrupoAnioCarrera WHERE Id = 1;
+SELECT * FROM GrupoAnioCarrera;
+
+-- Docente
+INSERT INTO Docente (Nombre, Apellidos, Dui, Cel, Huella, IdGrupo, IdCarrera) VALUES ('Docente1', 'Apellido1', '12345678-9', '77777777', 0x4D5A90000300000004000000FFFF0000, 1, 1);
+UPDATE Docente SET Nombre = 'Docente2' WHERE Id = 1;
+DELETE FROM Docente WHERE Id = 1;
+SELECT * FROM Docente;
+
+
+
+
+-- FASE 4: Creación de procedimientos almacenados
+
+
+
+-- FASE 5: Agregar registros a todas las tablas de la base de datos
+
+
+
+-- FASE 6: Creación de consultas SELECT con multi-tablas para reportes
---
+++ scripts/tables.sql
@@ -0,0 +1,83 @@
+CREATE DATABASE BDSysAsistenia
+USE BDSysAsistenia
+
+CREATE TABLE Grupo(
+	Id Tinyint Not Null Primary Key Identity(1,1),
+	Nombre Varchar(7) Not Null,
+);
+
+CREATE TABLE Anio(
+	Id Tinyint Not Null Primary Key Identity(1,1),
+	Nombre Varchar(7) Not Null,
+)
+
+CREATE TABLE GrupoAnio(
+	Id Tinyint Not Null Primary Key Identity(1,1),
+	IdGrupo Tinyint Not Null Foreign Key References Grupo(Id),
+	IdAnio Tinyint Not Null Foreign Key References Anio(Id)
+);
+
+CREATE TABLE Turno(
+	Id Tinyint Not Null Primary Key Identity(1,1),
+	Nombre Varchar(7) Not Null
+);
+
+CREATE TABLE Carrera(
+	Id Tinyint Not Null Primary Key Identity(1,1),
+	Nombre Varchar(50),
+	Siglas Varchar(7) Not Null
+);
+
+CREATE TABLE GrupoAnioCarrera(
+	Id Tinyint Not Null Primary Key Identity(1,1),
+	Codigo Varchar(10) Not Null,
+	EstudiantesMax TinyInt Not Null,
+	DiasPresencial bit Not Null,
+	IdCarrera Tinyint Not Null Foreign Key References Carrera(Id),
+	IdGrupoAnio Tinyint Not Null Foreign Key References GrupoAnio(Id)
+);
+
+CREATE TABLE Docente(
+	Id Int Not Null Primary Key Identity(1,1),
+	Nombre Varchar(20) Not Null, 
+	Apellidos Varchar(20) Not Null, 
+	Dui char(9) Null,
+	Cel char(9) Null,
+	Huella Varbinary(Max) Not Null,
+	IdGrupo Tinyint Not Null Foreign Key References GrupoAnioCarrera(Id),
+	IdCarrera Tinyint Not Null Foreign Key References Carrera(Id)
+);
+
+CREATE TABLE Estudiante(
+	Id Int Not Null Primary Key Identity(1,1),
+	Nombre Varchar(20) Not Null, 
+	Apellidos Varchar(20) Not Null, 
+	Dui char(9) Null,
+	Cel char(9) Null,
+	Huella Varbinary(Max) Not Null,
+	IdGrupo Tinyint Not Null Foreign Key References GrupoAnioCarrera(Id)
+);
+
+
+CREATE TABLE Nomenclatura(
+	Id Tinyint Not Null Primary Key Identity(1,1),
+	Letra char(1) Not Null,
+	Descripcion varchar(20) Not Null,
+);
+
+CREATE TABLE Criterio(
+	Id Tinyint Not Null Primary Key Identity(1,1),
+	Nombre varchar(20) Not Null, --18
+	Descripcion varchar(150) Not Null, --129
+);
+
+CREATE TABLE Asistencia(
+	Id Int Not Null Primary Key Identity(1,1),
+	IdEstudiante Int Not Null Foreign Key References Estudiante(Id),
+	IdDocente Int Not Null Foreign Key References Docente(Id),
+	IdCriterio Tinyint Not Null Foreign Key References Criterio(Id),
+	IdNomenclatura Tinyint Not Null Foreign Key References Nomenclatura(Id),
+	Observaciones varchar(50) Not Null,
+	Fecha DateTime Not Null
+	
+);
